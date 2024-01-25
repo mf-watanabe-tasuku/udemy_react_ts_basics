4bd7d9cbc9d8effd13f57f8c1e27b966e5242f3d
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-01-25 21:53:19 +0900

    Compolete NewGoal.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,12 @@ export type CourseGoal = {
 export default function App() {
   const [goals, setGoals] = useState<CourseGoal[]>([]);
 
-  function handleAddGoal() {
+  function handleAddGoal(goal: string, summary: string) {
     setGoals(prev => {
       const newGoal: CourseGoal = {
         id: Math.random(),
-        title: 'Learn React + TS',
-        description: 'Learn it in depth!'
+        title: goal,
+        description: summary
       };
 
       return [...prev, newGoal];
@@ -34,7 +34,7 @@ export default function App() {
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <NewGoal />
+      <NewGoal onAddGoal={handleAddGoal} />
       <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );
